7df556174a76147068c6bb17e32ea24b9091738d
Brennan Brown <Brennan.Brown@edu.sait.ca>
2020-02-23 12:52:45 -0700

    Add functionality and tests

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -2,9 +2,11 @@
 const functions = {
     
     size: (num) => {
+        if (num < 0) return "negative"
         if (num < 10) return "small";
         if (num < 20) return "medium";
-        return "large";
+        if (num < 1000) return "large"
+        return "very large";
     },
 
     add: (num1, num2) => {
@@ -13,9 +15,33 @@ const functions = {
 
     subtract: (num1, num2) => {
         return num1 - num2;
+    },
+
+    multiply: (num1, num2) => {
+        return num1 * num2;
+    },
+
+    divide: (num1, num2) => {
+        // Trunication required to
+        // change floats into ints.
+        return Math.trunc(num1 / num2);
+    },
+
+    largerNumber: (num1, num2) => {
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    },
+
+    powerOf: (num1, num2) => {
+        return Math.pow(num1, num2);
     }
 
     
 };
+
+
 
 export default functions;
