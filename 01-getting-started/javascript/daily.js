0dc9bc7291f1d6d0adf5119e0c3d8f9882c9f096
brennanbrown <brennan.brown@edu.sait.ca>
2020-03-04 15:47:31 -0700

    Add template files

---
+++ 01-getting-started/javascript/daily.js
@@ -0,0 +1,10 @@
+const functions = {
+
+    convertToFahrenheit: (celsiusTemp) =>  {
+        fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+        return fahrenheitTemp;
+    }
+
+}
+
+export default functions; 
