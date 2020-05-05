ebc095b918c0f06e3f71a2744da0c7f906917ddd
Brennan Brown <brennan.brown@edu.sait.ca>
2020-05-05 16:13:12 -0600

    Fix account details

--- 03-objects/src/scripts/account.js
+++ 03-objects/src/scripts/account.js
@@ -36,44 +36,6 @@ class AccountController {
         return myAccount.join('');
     }
 
-    deleteAcc(name) {
-        let myAccount = this.accs.find(x => x.name === name);
-        let i = this.accs.indexOf(myAcc);
-        this.accs.splice(i, 1);
-    }
-
-    accsTotal() {
-        let value = this.accs.map((x) => x.balance);
- 
-            value = value.reduce((a, b) => (Number(a) + Number(b)));
-            return '$' + Number.parseFloat(value).toFixed(2);
-    }
-
-    biggestAcc() {
-        this.accs.sort((a, b) => { return b.balance - a.balance });
-        let value = [this.accs[0].name, ' : $' + this.accs[0].balance.toFixed(2)];
-        return value.join('');
-    }
-
-    smallestAcc() {
-        this.accs.sort((a, b) => { return a.balance - b.balance });
-        let value = [this.accs[0].name, ' : $' + (this.accs[0].balance.toFixed(2))];
-        return value.join('');
-    }
-
-    isNewAcc(name) {
-        for (let v in this.accs) {
-            if (this.accs[v].name === name) {
-                return 'ERROR';
-            }
-        } return name;
-    }
-
-    isNewAmount(num) {
-        if (isNaN(num)) {
-            return 'ERROR';
-        } return num;
-    }
 }
 
 export { Account, AccountController };
