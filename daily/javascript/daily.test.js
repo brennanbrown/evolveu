2287ede3d5ad645db10b16781ea44e6790a99177
Brennan Brown <brennan.brown@edu.sait.ca>
2020-06-13 14:33:56 -0600

    Remove clutter files

--- daily/javascript/daily.test.js
+++
@@ -1,6 +0,0 @@
-import daily from './daily'
-
-test('Are these functions working?', () => {
-    expect(daily.convertToFahrenheit(0)).toBe(32);
-    expect(daily.convertToFahrenheit(100)).toBe(212);
-});
